d4254a85646cf4755c6f883b55c8c6d5f872a25a
Wojtek <wojtek.kobier@wp.pl>
2024-03-17 21:22:28 +0100

    First Steps

---
+++ ToDoApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace todoapp
+{
+    internal class Program
+    {
+        static List<Task> AllTasks = new List<Task>();
+
+        public static void Main(string[] args)
+        {
+            AddTask(1, "Posprzątać pokój", "poodkurzać");
+            ShowAllTasks();
+        }
+
+        public static void AddTask(int id, string taskName, string taskDescription)
+        {
+            Task newTask = new Task(id, taskName, taskDescription);
+            AllTasks.Add(newTask);
+        }
+        /*
+                public void AddTaskDescription()
+                {
+
+                }
+
+                public void RemoveTask()
+                {
+
+                }*/
+
+        public static void ShowAllTasks()
+        {
+            foreach (var task in AllTasks)
+            {
+                Console.WriteLine($"Id {task.Id} Nazwa {task.TaskName} Opis {task.TaskDescription}");
+            }
+        }
+    }
+}
---
+++ ToDoApp/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todoapp
+{
+    internal class Task
+    {
+
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+
+        public int Id { get; set; }
+
+        public Task(int id, string taskName, string taskDescription)
+        {
+            this.Id = id;
+            this.TaskName = taskName;
+            this.TaskDescription = taskDescription;
+
+        }
+
+
+    }
+}
